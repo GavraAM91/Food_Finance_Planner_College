0e0146672493efd504e1718bc01c87b59405d448
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2025-12-13 21:34:01 +0700

    blok fungsi performCurlrequest karena tidak terpakai

--- src/backend/ConnectionAPI.cpp
+++ src/backend/ConnectionAPI.cpp
@@ -16,24 +16,25 @@ size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
     return size * nmemb;
 }
 
+// TIDAK DIPAAKAI
 // FUNGSI UNTUK MENERIMA CURL REQUEST
-bool ConnectionAPI::performCurlRequest(const string &url, string &response)
-{
-    CURL *curl = curl_easy_init();
-    if (!curl)
-    {
-        return false;
-    }
+// bool ConnectionAPI::performCurlRequest(const string &url, string &response)
+// {
+//     CURL *curl = curl_easy_init();
+//     if (!curl)
+//     {
+//         return false;
+//     }
 
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, response); // menangkap respon
+//     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+//     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+//     curl_easy_setopt(curl, CURLOPT_WRITEDATA, response); // menangkap respon
 
-    CURLcode res = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
+//     CURLcode res = curl_easy_perform(curl);
+//     curl_easy_cleanup(curl);
 
-    return (res == CURLE_OK);
-}
+//     return (res == CURLE_OK);
+// }
 
 // MENGIRIM DATA KE N8N
 bool ConnectionAPI::postJSON(string &jsonData, string *response)
